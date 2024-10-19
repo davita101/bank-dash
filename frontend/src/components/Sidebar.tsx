dab0041698a12c12d096dd1e12439c2679f8660d
tornikesirabidze <tornikesirabidze17@gmail.com>
2024-10-19 09:27:14 +0300

    icon add

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaCreditCard, FaLightbulb } from 'react-icons/fa'
+import { FaHandHoldingDollar } from 'react-icons/fa6'
 import { GrTransaction } from 'react-icons/gr'
 import { HiMiniHome } from 'react-icons/hi2'
+import { MdAccountCircle, MdDesignServices } from 'react-icons/md'
+import { RiSettings5Fill } from 'react-icons/ri'
 
 const Sidebar = () => {
     const bot = [
@@ -12,6 +16,30 @@ const Sidebar = () => {
         {
             name: "transactions",
             icon: GrTransaction 
+        },
+        {
+            name: "Accounts",
+            icon: MdAccountCircle  
+        },
+        {
+            name: "Creditcard",
+            icon: FaCreditCard   
+        },
+        {
+            name: "Loans",
+            icon: FaHandHoldingDollar     
+        },
+        {
+            name: "Services",
+            icon: MdDesignServices     
+        },
+        {
+            name: "Privileges",
+            icon: FaLightbulb      
+        }
+        {
+            name: "Settings",
+            icon: RiSettings5Fill       
         }
     ]
     // classwork install icons
